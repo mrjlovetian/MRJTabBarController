821e7113bb9db00ea1ddfd8d98407a1fe2fc7e99
MRJ <mrjyuhongjiang@gmail.com>
2017-08-11 15:50:27 +0800

    create file for tableViewController

---
+++ Example/Pods/Target Support Files/Pods-TableBarViewController_Example/Pods-TableBarViewController_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TableBarViewController_Example : NSObject
+@end
+@implementation PodsDummy_Pods_TableBarViewController_Example
+@end
---
+++ Example/Pods/Target Support Files/TableBarViewController/TableBarViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TableBarViewController : NSObject
+@end
+@implementation PodsDummy_TableBarViewController
+@end
